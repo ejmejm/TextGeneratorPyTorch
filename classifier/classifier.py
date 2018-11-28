611535f1ae2d6435df28d2361b69cae258c535d3
ejmejm <ninjaedan@aol.com>
2018-11-28 17:01:14 -0500

    classifier module done

---
+++ classifier/classifier.py
@@ -0,0 +1,38 @@
+import pickle
+from keras.models import load_model
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from keras.preprocessing import sequence
+
+class Classifier():
+    def __init__(self):
+        with open('tokenizer', 'rb') as f:
+            self.tokenizer = pickle.load(f)
+        self.model = load_model('model.hdf5')
+        self.formatter = RegexpTokenizer(r'\w+')
+        self.stop_words = set(stopwords.words('english'))
+        self.max_seq_len = 20
+
+    def format_sent(self, sent):
+        return ' '.join([word for word in self.formatter.tokenize(sent) 
+                            if word not in self.stop_words])
+
+    def preprocess_sent(self, sent):
+        seq = self.tokenizer.texts_to_sequences([sent])
+        padded_seq = sequence.pad_sequences(seq, maxlen=self.max_seq_len)
+        return padded_seq
+
+    def classify(self, x):
+        return self.model.predict(x)[0]
+
+    def prediction_to_str(self, pred):
+        if pred[0] > pred[1]:
+            return 'left'
+        return 'right'
+
+    def classify_sentence(self, sent):
+        formatted_sent = self.format_sent(sent)
+        preprocessed_sent = self.preprocess_sent(formatted_sent)
+        pred = self.classify(preprocessed_sent)
+        final_class = self.prediction_to_str(pred)
+        return final_class
